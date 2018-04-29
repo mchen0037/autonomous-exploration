05b312396c533aa603557d7071dde35d218aeea8
Mighty Chen <mchen73@ucmerced.edu>
2018-04-28 20:53:41 -0700

    cleaned up code and merged graph.cpp to discreteMap.cpp'

--- finalnodes/src/discreteMap.cpp
+++ finalnodes/src/discreteMap.cpp
@@ -1,71 +1,73 @@
 #include <ros/ros.h>
-#include <logical_camera_plugin/logicalImage.h>
-#include <tf/transform_datatypes.h>
-#include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/utils.h>
 #include <nav_msgs/OccupancyGrid.h>
-#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/Pose.h>
 #include <angles/angles.h>
 #include <map>
-//#include <tf/Matrix3x3.h>
 
-
-using namespace std;
+std::vector <int> graph;
+geometry_msgs::Pose current_pose;
 
 int matToArr(int row, int col, int row_tar, int col_tar){
   return (row_tar-1) * col + col_tar - 1;
 
 }
 
+float roundTenth(float a) {
+  return round(10 * (a)) / 10;
+}
+
 void discretize(const nav_msgs::OccupancyGrid msg){
   int square = 10; // square x square
 //gets me to the start of every square
-  vector <int> graph;
-  for(int j = 200; j < 600; j+=square){
-    for(int i = 200; i < 600;i+=square){
 
-
-
+  for (int j = 200; j < 600; j+=square) {
+    for (int i = 200; i < 600;i+=square) {
       int count = 0;
-      for(int l = 0; l < square; l++){
-        for(int k = 0; k < square; k++){
-      
-          if((int)msg.data[matToArr(800,800,j+l,i+k)] != 0 && count < 1){
+      for(int l = 0; l < square; l++) {
+        for(int k = 0; k < square; k++) {
+          if((int)msg.data[matToArr(800,800,j+l,i+k)] != 0 && count < 1) {
             count++;
-          } 
+          }
         }
       }
-
-
       graph.push_back(count);
-
-
     }
   }
 
   for(int i =0; i < graph.size(); i++){
-    cout << graph[i] << "\t";
-    
+    std::cout << graph[i] << "\t";
+
     if ( (i+1)%(400/square) == 0 ){
-      cout <<"\n\n";
+      std::cout <<"\n\n";
     }
  }
+}
 
+void subPose(const geometry_msgs::Pose msg) {
+  current_pose = msg;
+  float angle = tf2::getYaw(msg.orientation);
+  angle = angles::normalize_angle(angle);
+  float seeUpToX = 2 * cos(angle) + current_pose.position.x;
+  float seeUpToY = 2 * sin(angle) + current_pose.position.y;
+
+  float rounded_current_X = roundTenth(current_pose.position.x);
+  float rounded_current_Y = roundTenth(current_pose.position.y);
+
+  std::cout << "I can see from " << rounded_current_X << "up to " << roundTenth(seeUpToX)
+    << " and " << rounded_current_Y << " to " << roundTenth(seeUpToY) << "\n";
 }
 
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "discreteMap");
   ros::NodeHandle nh;
-
   ros::Rate rate(20);
-
   ros::Subscriber sub = nh.subscribe<nav_msgs::OccupancyGrid>("/map", 1000, discretize);
-
+  ros::Subscriber sub2 = nh.subscribe<geometry_msgs::Pose>("perfect_localization", 1000, subPose);
 
   while (ros::ok()) {
     ros::spinOnce();
   }
 
 }
-
